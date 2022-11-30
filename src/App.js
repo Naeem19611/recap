e4844ed328c98dba6df1308e80fc032015ba87a2
Naeem19611 <naeem.mizan143@gmail.com>
2022-11-30 22:15:26 +0600

    react recap practise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+function App () {
+  const[nayoks, setNayoks] = useState([])
+  useEffect (() => {
+    fetch ('https://jsonplaceholder.typicode.com/users')
+    .then (res => res.json())
+    .then (data => setNayoks(data))
+
+  }, [])
+
+
+
+  //  const nayoks = [{name:'Deepjol' ,age: 44},{name:'BappaRaz',age: 30},{name:'alomgir',age: 55},{name:'omor sani',age: 48}, {name:'razzak',age: 80}];
   return (
     <div className="App">
+      <MovieCounter></MovieCounter>
+     {
+      nayoks.map (nk => <Nayok name={nk.name} key={nk.id} age={nk.age}></Nayok>)
+     }
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
+
+    }
+function MovieCounter () {
+  const [count,setCount] = useState(0);
+
+  const handleClick = () => setCount(count + 1);
+  return (
+    <div>
+      <button onClick = {handleClick}>Add Movies</button>
+      <h3>Number of movies: {count}</h3>
+      <MovieDisplay movies={count}></MovieDisplay>
+      <MovieDisplay movies={count + 10}></MovieDisplay>
+      <MovieDisplay movies={count}></MovieDisplay>
+      <MovieDisplay movies={count}></MovieDisplay>
+    </div>
+  )
+}
+
+function MovieDisplay (props) {
+  return <h3>I have moviedisplay acted: {props.movies}</h3>
+}
+
+function Nayok (props) {
+  const nayokStyle = {
+    border: '2px solid black',
+    backgroundColor : 'crimson',
+    color : 'white',
+    margin : '10px'
+  }
+  return (
+    <div style={nayokStyle}>
+        <h1>I am React - {props.name}</h1>
+        <h3>React Basic in {props.age || 30} years</h3>
+      </div>
+    )
 }
 
 export default App;
